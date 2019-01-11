a850d7fe8bc7edc3052c367fc38b90d1644e53db
Nick <dambergn@gmail.com>
2019-01-11 04:57:02 -0800

    menue

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # mhz-tools
 Linux administration tools
+
+## Update
+
+## System Information
+
+## Battery Information
+
+## Screen Settings
+
+## Change Log
+### 2019-01-11
+- Started github repo
--- menue.sh
+++ menue.sh
@@ -7,15 +7,17 @@ echo ' | \  / | |__| |  / /| (___  \ \_/ / (___  '
 echo ' | |\/| |  __  | / /  \___ \  \   / \___ \ '
 echo ' | |  | | |  | |/ /__ ____) |  | |  ____) |'
 echo ' |_|  |_|_|  |_/_____|_____/   |_| |_____/ '
-echo '         MEGAHURTZ SYSTEMS MENUE '
-echo '           version v0.1.2 beta'
+echo '          MEGAHURTZ SYSTEMS MENU '
+echo '           version v0.1.3 beta'
 sleep 1
 
 echo 'Tools options'
-echo 'update'
-echo 'sysinfo'
-echo 'battery'
-echo 'screen'
+echo '**********'
+echo '*update  *'
+echo '*sysinfo *'
+echo '*battery *'
+echo '*screen  *'
+echo '**********'
 
 echo ''
 read option
