26c78491aa23525f7a50b85bf2c7e7325094f242
Nick <dambergn@gmail.com>
2019-01-12 00:53:43 -0800

    return to main menu option

--- screen.sh
+++ screen.sh
@@ -17,3 +17,19 @@ xrandr -o normal
 
 clear
 echo 'Screen has been fixed, have a nice day.'
+
+# Return to main menu
+echo 'press enter to return to menu'
+while true
+do
+    read -s -n 1 key  # -s: do not echo input character. -n 1: read only 1 character (separate with space)
+
+# double brackets to test, single equals sign, empty string for just 'enter' in this case...
+# if [[ ... ]] is followed by semicolon and 'then' keyword
+    if [[ $key = "" ]]; then 
+        # echo 'You pressed enter!'
+        ./menue.sh
+    else
+        echo "'$key' is not a valid option."
+    fi
+done
