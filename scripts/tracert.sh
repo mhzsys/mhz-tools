825fc9d0dd17816866cb8e0242aaacca6b1929aa
Nick <dambergn@gmail.com>
2019-01-18 03:57:47 -0800

    updated header and added treace route program

---
+++ scripts/tracert.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# echo '  __  __ _    _ ______ _______     _______ '
+# echo ' |  \/  | |  | |___  // ____\ \   / / ____|'
+# echo ' | \  / | |__| |  / /| (___  \ \_/ / (___  '
+# echo ' | |\/| |  __  | / /  \___ \  \   / \___ \ '
+# echo ' | |  | | |  | |/ /__ ____) |  | |  ____) |'
+# echo ' |_|  |_|_|  |_/_____|_____/   |_| |_____/ '
+./scripts/header-mhzsys.sh
+echo '                Trace Route '
+sleep 1
+
+echo 'please enter the IP address or URL you would like to trace.'
+
+read totrace
+
+mtr -t $totrace
+
+./menu.sh
