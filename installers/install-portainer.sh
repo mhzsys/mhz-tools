f890f4dcec22d805599562bd5e7b1bac4d375894
Nicholas Damberg <dambergn@gmail.com>
2026-02-28 23:57:52 -0600

    Update install-portainer.sh
    updated portainer version

--- installers/install-portainer.sh
+++ installers/install-portainer.sh
@@ -20,7 +20,7 @@ function is_docker_installed() {
         sudo docker volume create portainer_data
 
         echo 'Setting up and running docker container'
-        sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.33.2
+        sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.39.0
 
         echo 'Portainer Install Complete'
         return 0
